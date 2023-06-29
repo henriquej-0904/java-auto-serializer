91f291b0e29f358318d0c73ef33ebb69e578dd3e
ajccosta <aj.costa@campus.fct.unl.pt>
2023-06-29 15:11:26 +0100

    added base prototype
    starting from https://github.com/pfouto/babel-tutorial baseline

---
+++ run_client.sh
@@ -0,0 +1,1 @@
+java -Dlog4j2.configurationFile=log4j2.xml -jar target/A-PingPong.jar interface=lo n_pings=1 target_address=127.0.0.1 message="hello"
---
+++ run_server.sh
@@ -0,0 +1,1 @@
+java -Dlog4j2.configurationFile=log4j2.xml -jar target/A-PingPong.jar interface=lo
